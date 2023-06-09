b7302e885ff29c763a2d9058b60934d43262fde3
HasanbasriUysal <48217@edu.raseko.fi>
2023-06-09 14:57:57 +0300

    ensimmainen testaus

---
+++ test_timetools.py
@@ -0,0 +1,21 @@
+# For testing the timetools module 
+
+# Libraries and modules 
+import unittest
+import timetools
+
+#Test classes
+class test_time(unittest.TestCase):
+
+    def test_datediff(self):
+        expected_result = (3, "days")
+        result = timetools.datediff("2023-06-09","2023-06-06")
+        self.assertEqual(result,expected_result)
+
+    def test_timediff2(self):
+        result = timetools.timediff2("18:00:00","23:30:00","minute")
+        expected_result = (330)
+        self.assertEqual(result,expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
